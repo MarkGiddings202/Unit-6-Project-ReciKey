cb5cd2f252f47c0cfc07ba8733121e8f3c27b236
Crislp12 <cristopher.picopinos@gmail.com>
2022-09-20 16:12:48 -0400

    function: event listener can create menu items

--- elements.js
+++ elements.js
@@ -1,5 +1,4 @@
 // Dom Elements
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-btn");
-
 const mealSearchResults = document.getElementById("meal");
--- functions.js
+++ functions.js
@@ -11,10 +11,29 @@ async function fetchingMealByIngredient(ingredient) {
     } else {
         console.log(json);
         for (let i = 0; i < json.meals.length; i++) {
+            const card = document.createElement("div")
             const mealHeading = document.createElement("h2");
-            
-            mealSearchResults.after(mealHeading);
+            const mealImg= document .createElement("img")
+            ///  apend elements 
+            mealSearchResults.appendChild(card) // parent div meal
+            card.appendChild(mealImg)
+            card.appendChild(mealHeading)
             mealHeading.innerText = json.meals[i].strMeal;
+           
+            /// fecth for recepi 
+            const recepiceFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${json.meals[i].idMeal}`)
+            const recepicejson = await recepiceFetch.json()
+            
+            // style for images
+            mealImg.src = recepicejson.meals[0].strMealThumb
+            mealImg.width ="200"
+            mealImg.height ="200"
+            mealImg.id = "babab"
+
+
+            // const mealHeading = document.createElement("h2");
+            // mealSearchResults.after(mealHeading);
+            // mealHeading.innerText = json.meals[i].strMeal;
         }
     }
 }
