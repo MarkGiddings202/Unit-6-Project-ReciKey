6660c9e573e71de2ebdd12018cd9754e03bd83be
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-09-21 16:02:41 -0400

    added image functionality to meal sections, also case for invalid search

--- functions.js
+++ functions.js
@@ -4,11 +4,10 @@ const removeChilds = (parent) => {
         parent.removeChild(parent.lastChild);
     }
 };
-async function getMealImg (link){
+async function getMealImg(link) {
     const recepiceFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${link}`)
     const recepicejson = await recepiceFetch.json()
     return recepicejson.meals[0].strMealThumb
-
 }
 
 async function fetchingMealByIngredient(ingredient) {
@@ -16,21 +15,21 @@ async function fetchingMealByIngredient(ingredient) {
     const json = await response.json();
 
     if (!json.meals) {
-        console.log("There are no meals for this ingredient")
+        resultsTitle.innerText = "No meals found...";
     } else {
         console.log(json);
-        
+
         removeChilds(mealSearchResults);
 
         for (let i = 0; i < json.meals.length; i++) {
             const card = document.createElement("div")
-            card.id ="card"
+            card.id = "card"
             const mealHeading = document.createElement("h2");
-            const mealImg= document.createElement("img")
+            const mealImg = document.createElement("img")
             mealImg.id = "meal-Img"
 
 
-            
+
             ///  apend elements 
             mealSearchResults.appendChild(card) // parent div meal
             card.appendChild(mealImg)
@@ -39,15 +38,14 @@ async function fetchingMealByIngredient(ingredient) {
             console.log(json.meals[i])
 
 
-           const img = await getMealImg(json.meals[i].idMeal)
-            
+            const img = await getMealImg(json.meals[i].idMeal)
+
             // style for images
             mealImg.src = img
             mealImg.width = "200"
-            mealImg.height ="200"
-        
+            mealImg.height = "200"
         }
-   }
+    }
 }
 
 async function listMealsByCategory(category) {
@@ -76,11 +74,26 @@ async function listMealsByCategory(category) {
     removeChilds(mealSearchResults);
 
     for (let i = 0; i < json.meals.length; i++) {
-        const newMeal = document.createElement("h3");
+            const card = document.createElement("div")
+            card.id = "card"
+            const mealHeading = document.createElement("h2");
+            const mealImg = document.createElement("img")
+            mealImg.id = "meal-Img"
 
-        mealSearchResults.appendChild(newMeal);
-        newMeal.innerText = json.meals[i].strMeal;
-    }
+            ///  apend elements 
+            mealSearchResults.appendChild(card) // parent div meal
+            card.appendChild(mealImg)
+            card.appendChild(mealHeading)
+            mealHeading.innerText = json.meals[i].strMeal;
+            console.log(json.meals[i])
+
+            const img = await getMealImg(json.meals[i].idMeal)
+
+            // style for images
+            mealImg.src = img
+            mealImg.width = "200"
+            mealImg.height = "200"
+        }
 }
 
 
