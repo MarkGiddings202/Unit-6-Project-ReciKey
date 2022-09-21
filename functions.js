4d5e0ea9aa43ae27337547774b3c996c45116cc0
Crislp12 <cristopher.picopinos@gmail.com>
2022-09-21 10:24:18 -0400

    fucntion: added helper fucntion

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 // Functions
 
+async function getMealImg (link){
+    const recepiceFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${link}`)
+    const recepicejson = await recepiceFetch.json()
+    return recepicejson.meals[0].strMealThumb
+
+}
+
 async function fetchingMealByIngredient(ingredient) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
     const json = await response.json();
@@ -12,31 +19,33 @@ async function fetchingMealByIngredient(ingredient) {
         console.log(json);
         for (let i = 0; i < json.meals.length; i++) {
             const card = document.createElement("div")
+            card.id ="card"
             const mealHeading = document.createElement("h2");
-            const mealImg= document .createElement("img")
+            const mealImg= document.createElement("img")
+            mealImg.id = "meal-Img"
+
+
+            
             ///  apend elements 
             mealSearchResults.appendChild(card) // parent div meal
             card.appendChild(mealImg)
             card.appendChild(mealHeading)
             mealHeading.innerText = json.meals[i].strMeal;
-           
-            /// fecth for recepi 
-            const recepiceFetch = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${json.meals[i].idMeal}`)
-            const recepicejson = await recepiceFetch.json()
+            console.log(json.meals[i])
+
+
+           const img = await getMealImg(json.meals[i].idMeal)
             
             // style for images
-            mealImg.src = recepicejson.meals[0].strMealThumb
-            mealImg.width ="200"
+            mealImg.src = img
+            mealImg.width = "200"
             mealImg.height ="200"
-            mealImg.id = "babab"
-
-
-            // const mealHeading = document.createElement("h2");
-            // mealSearchResults.after(mealHeading);
-            // mealHeading.innerText = json.meals[i].strMeal;
+        
         }
     }
 }
+
+
 
 // fetchingMealByIngredient("chicken breast")
 
