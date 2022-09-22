29f6feabdca5fae5a521d17c97a5f888c434e025
Crislp12 <cristopher.picopinos@gmail.com>
2022-09-22 14:08:35 -0400

    style: added a Modal feture on recipe cards

--- elements.js
+++ elements.js
@@ -17,10 +17,20 @@ const goatOptionBtn = query("#goat-option");
 const vegetarianOptionBtn = query("#vegetarian-option");
 const starterOptionBtn = query("#starter-option");
 const miscellaneousOptionBtn = query("#miscellaneous-option");
-
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-btn");
 
 const mealSearchResults = document.getElementById("meal");
 const resultsTitle = document.querySelector("#Results-title")
+
+const recipeSearcgBtn = document.querySelectorAll(".Recipe-Searcg-Btn")
+const recipeTitle = document.querySelector(".recipe-title")
+const recipeInstructions = document.querySelector(".instruct")
+const recipeImg = document.querySelector(".recipe-img")
+
+const exitButton = document.querySelector("#Exit-button")
+const mealDetailsContent = document.querySelector(".meal-details-content")
+const span = document.getElementsByClassName("close")[0];
+const container = document.querySelector(".Container")
+const pageBody = document.querySelector(".Body")
 
