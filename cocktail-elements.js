f728f41c10f4cf5e3bde75d34ba3ffe44a89cb46
Gavin Giddings <Gavin>
2022-09-23 09:29:36 -0400

    Added cocktail.html, added cocktail-function.js, added cocktail-elements.js added cocktail.css

---
+++ cocktail-elements.js
@@ -0,0 +1,4 @@
+let userInput = document.getElementById("user-input");
+let userBtn = document.getElementById("cocktail-btn");
+let cocktailDiv = document.getElementById("cocktail");
+let cocktailH2 = document.getElementById("Results-title");
